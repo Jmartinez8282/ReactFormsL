8167693b1da7bbb6a3e3d82c690936a27db8028f
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-05-24 08:58:51 -0700

    Forms Validation , reack forms hook, zod

--- react_formsl/src/components/Forms/ReactFormsExamp.tsx
+++ react_formsl/src/components/Forms/ReactFormsExamp.tsx
@@ -6,8 +6,8 @@ import { FieldValues,useForm } from "react-hook-form"
 
 const ReactFormsExamp = () => {
 
-const {register,handleSubmit} = useForm()
-console.log(register('name'));
+const {register,handleSubmit,formState:{errors}} = useForm()
+console.log(errors);
 
 const onHelpSubmit = (data:FieldValues) => {
     console.log(data);
@@ -20,9 +20,13 @@ const onHelpSubmit = (data:FieldValues) => {
       <form onSubmit={handleSubmit(onHelpSubmit)}> 
         <div className="mb-3 myContainer">
           <label htmlFor="" className="form-label">Name</label>
-          <input {...register('name')} id="name" type="text" className="form-control" />
+          <input {...register('name',{required: true, minLength: 3})} id="name" type="text" className="form-control" />
+          {errors.name?.type === 'required' && <p className="text-danger">The name field is required</p>}
+          {errors.name?.type === 'minLength' ? <p className="text-danger">The name must be at least 3 charaters long</p>: null}
           <label  htmlFor="" className="form-label">Age</label>
-          <input {...register('age')}  id="age" type="number" className="form-control" />
+          <input {...register('age',{required:true,minLength: 1})}  id="age" type="number" className="form-control" />
+          {errors.age?.type === 'required' ? <p className="text-danger">The age field is required</p>: null}
+         
         <button className=" mt-3 btn btn-primary" type="submit">Submit</button>
         </div>
       </form>
